594ef77733ba1e4af65d379a351ee8261fe6d0fd
mattheweallen <mattheweallen@gmail.com>
2013-11-06 14:09:35 -0600

    latest version

--- athleticgis/src/com/athleticgis/DashboardBean.java
+++ athleticgis/src/com/athleticgis/DashboardBean.java
@@ -8,5 +8,12 @@ import java.io.Serializable;
 @SessionScoped
 public class DashboardBean implements Serializable {
 	private static final long serialVersionUID = 1L;
+	private String activityName;
 	
+	public String getActivityName() {
+		return activityName;
+	}
+	public void setActivityName(String activityName) {
+		this.activityName = activityName;
+	}
 }
