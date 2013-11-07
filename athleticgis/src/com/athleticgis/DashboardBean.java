d0d968681022eabe754036d3f5da85eb817cbbe5
mattheweallen <mattheweallen@gmail.com>
2013-11-07 07:54:34 -0600

    Mockup

--- athleticgis/src/com/athleticgis/DashboardBean.java
+++ athleticgis/src/com/athleticgis/DashboardBean.java
@@ -6,6 +6,9 @@ import javax.faces.bean.SessionScoped;
 import com.athleticgis.model.Activity;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
  
 @ManagedBean
@@ -15,9 +18,15 @@ public class DashboardBean implements Serializable {
 	private List<Activity> activities;
 	
 	public List<Activity> getActivities() {
+		//hard coded
+		activities = new ArrayList<Activity>();
+		Activity a = new Activity();
+		a.setDate(new Timestamp((new Date()).getTime()));
+		a.setName("Tuesday Night World Championships");
+		activities.add(a);
 		return activities;
 	}
-	public void setActivityName(List<Activity> activities) {
-		this.activities = activities;
-	}
+//	public void setActivityName(List<Activity> activities) {
+//		this.activities = activities;
+//	}
 }
