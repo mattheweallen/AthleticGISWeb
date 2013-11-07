62a5900f10e79517e66196383b186d5ad56b527d
mattheweallen <mattheweallen@gmail.com>
2013-11-07 09:04:47 -0600

    File Upload Stuff

--- athleticgis/src/com/athleticgis/DashboardBean.java
+++ athleticgis/src/com/athleticgis/DashboardBean.java
@@ -5,12 +5,16 @@ import javax.faces.bean.SessionScoped;
 
 import com.athleticgis.model.Activity;
 
+import java.io.File;
 import java.io.Serializable;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
- 
+
+import org.richfaces.event.FileUploadEvent;
+import org.richfaces.model.UploadedFile;
+
 @ManagedBean
 @SessionScoped
 public class DashboardBean implements Serializable {
@@ -25,6 +29,16 @@ public class DashboardBean implements Serializable {
 		a.setName("Tuesday Night World Championships");
 		activities.add(a);
 		return activities;
+	}
+	
+	public void fileUploadListener(FileUploadEvent event) throws Exception {
+		UploadedFile item = event.getUploadedFile();
+//	    File file = new File();
+//	    file.setLength(item.getData().length);
+//	    file.setName(item.getName());
+//	    file.setData(item.getData());
+//	    files.add(file);
+//	    uploadsAvailable--;
 	}
 //	public void setActivityName(List<Activity> activities) {
 //		this.activities = activities;
